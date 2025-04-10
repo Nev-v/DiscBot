26fbd64dbed51e90eb950708d73a4ece02c3558b
Nev-v <toresson.malte@gmail.com>
2025-04-10 17:19:49 +0200

    Join channel command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import discord
-from discord.ext import commands
-from discord import app_commands
+import discord # type: ignore
+from discord.ext import commands # type: ignore
 import sys
 import os
 
@@ -38,12 +37,13 @@ intents.messages = True
 intents.message_content = True
 
 client = Client(command_prefix="!", intents=intents)
-client.run(const.token)
 
 GUILD_ID = discord.Object(id=286827966783029248)
 
 @client.tree.command(name="soundboard", description="Play a sound", guild=GUILD_ID)
 async def join(ctx):
-    if ctx.author.voice:
-        channel = ctx.author.voice.channel
+    if ctx.user.voice:
+        channel = ctx.user.voice.channel
         await channel.connect()
+
+client.run(const.token)
