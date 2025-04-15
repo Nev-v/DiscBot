605c7d5d308d838ac843a249588654085621d617
Nev-v <toresson.malte@gmail.com>
2025-04-15 18:40:48 +0200

    Updated fishing, added shop

--- main.py
+++ main.py
@@ -183,15 +183,10 @@ async def work(interaction: discord.Interaction):
         await interaction.response.send_message("You have already worked today!", ephemeral=True)
 
 @client.tree.command(name="fish", description="Reel in some cash", guild=GUILD_ID)
-@app_commands.describe(action="Action", sub_action="Optional action")
-@app_commands.choices(action=[app_commands.Choice(name="Fish", value="fish"), app_commands.Choice(name="Inventory", value="inv"), app_commands.Choice(name="Sell", value="sell")], sub_action=[app_commands.Choice(name="None", value="none"), app_commands.Choice(name="High Risk", value="highrisk")])
-async def angling(interaction: discord.Interaction, action: app_commands.Choice[str], sub_action: app_commands.Choice[str]):
+@app_commands.describe(bait="Bait")
+@app_commands.choices(bait=[app_commands.Choice(name="No Bait", value="nb"), app_commands.Choice(name="Basic Bait", value="bb"), app_commands.Choice(name="Advanced Bait", value="ab"), app_commands.Choice(name="Master Bait", value="mab"), app_commands.Choice(name="Mystery Bait", value="myb")])
+async def angling(interaction: discord.Interaction, bait: app_commands.Choice[str]):
     checkExist(interaction.user.id)
-
-    fish_common = "🐟"
-    fish_uncommon = "🐠"
-    fish_rare = "🐡"
-    fish_leg = "🐙"
 
     fish_rarity = {
         "🐟": "Common",
@@ -203,78 +198,88 @@ async def angling(interaction: discord.Interaction, action: app_commands.Choice[
     line = "🎣"
     water = "🟦"
 
-    match action.value:
-        case "fish":
-            emoji = [] 
-            hook_index = const.fish_minigame_width * const.line_length + const.line_x
-            i = 0
-            j = 0
+    emoji = [] 
+    hook_index = const.fish_minigame_width * const.line_length + const.line_x
+    i = 0
+    j = 0
+    match bait.value:
+        case "nb":
+            fish = random.choices([const.fish_common, const.fish_uncommon, const.fish_rare, const.fish_leg], weights=[8000, 1500, 490, 10], k=1)[0]
+        case "bb":
+            if checkReq(interaction.user.id, 1, "Bait", "Basic Bait"):
+                fish = random.choices([const.fish_common, const.fish_uncommon, const.fish_rare, const.fish_leg], weights=[370, 500, 120, 10], k=1)[0]
+            else:
+                await sendMessageWithExpiration(interaction, "You have no bait", 10)
+                return
+        case "ab":
+            if checkReq(interaction.user.id, 1, "Bait", "Advanced Bait"):
+                fish = random.choices([const.fish_common, const.fish_uncommon, const.fish_rare, const.fish_leg], weights=[210, 420, 310, 30], k=1)[0]
+            else:
+                await sendMessageWithExpiration(interaction, "You have no bait", 10)
+                return
+        case "mab":
+            if checkReq(interaction.user.id, 1, "Bait", "Master Bait"):
+                fish = random.choices([const.fish_common, const.fish_uncommon, const.fish_rare, const.fish_leg], weights=[100, 400, 400, 100], k=1)[0]
+            else:
+                await sendMessageWithExpiration(interaction, "You have no bait", 10)
+                return
+        case _:
+            await sendMessageWithExpiration(interaction, "Invalid Bait", 10)
 
-            fish = random.choices([fish_common, fish_uncommon, fish_rare, fish_leg], weights=[100, 50, 20, 5], k=1)[0]
+    emoji_str = ""
+    for i in range(const.fish_minigame_height):
+        j = 0
+        for j in range(const.fish_minigame_width):
+            if i == const.fish_y and j == const.fish_x:
+                emoji.append(fish)
+            elif j == const.line_x and i < const.line_length:
+                emoji.append(line)
+            else:
+                emoji.append(water)
+            j += 1
+        i += 1
 
-            emoji_str = ""
-            for i in range(const.fish_minigame_height):
-                j = 0
-                for j in range(const.fish_minigame_width):
-                    if i == const.fish_y and j == const.fish_x:
-                        emoji.append(fish)
-                    elif j == const.line_x and i < const.line_length:
-                        emoji.append(line)
-                    else:
-                        emoji.append(water)
-                    j += 1
-                i += 1
+    emoji_str = "\n".join(
+        "".join(emoji[i * const.fish_minigame_width:(i + 1) * const.fish_minigame_width])
+        for i in range(const.fish_minigame_height)
+    )
 
-            emoji_str = "\n".join(
-                "".join(emoji[i * const.fish_minigame_width:(i + 1) * const.fish_minigame_width])
-                for i in range(const.fish_minigame_height)
-            )
+    fish_index = emoji.index(fish)
+    fish_state = Fish(fish_index, fish)
+    view = ReelIn(fish_state, hook_index, interaction.user, fish_rarity)
 
-            fish_index = emoji.index(fish)
-            fish_state = Fish(fish_index, fish)
-            view = ReelIn(fish_state, hook_index, interaction.user, fish_rarity)
+    await interaction.response.send_message(emoji_str, view=view)
+    msg = await interaction.original_response()
 
-            await interaction.response.send_message(emoji_str, view=view)
-            msg = await interaction.original_response()
+    while fish_state.fish_index - const.fish_y * const.fish_minigame_width > 0 and view.active:
 
-            while fish_state.fish_index - const.fish_y * const.fish_minigame_width > 0 and view.active:
+        if fish_state.fish_index % const.fish_minigame_width > 0:
+            left_weight = (20 + (fish_state.fish_index % const.fish_minigame_width) * 5)**1.8
+            right_weight = (100 - (fish_state.fish_index % const.fish_minigame_width) * 5)**1.8
 
-                if fish_state.fish_index % const.fish_minigame_width > 0:
-                    left_weight = (20 + (fish_state.fish_index % const.fish_minigame_width) * 5)^2
-                    right_weight = (100 - (fish_state.fish_index % const.fish_minigame_width) * 5)^2
+            if fish_state.fish_index < ((const.fish_y+1) * const.fish_minigame_width) - 1:
+                movement = random.choices([-1, 0, 1], weights=[left_weight, 10, right_weight], k=1)[0]
+            else:
+                movement = -1
 
-                    if fish_state.fish_index < ((const.fish_y+1) * const.fish_minigame_width) - 1:
-                        movement = random.choices([-1, 0, 1], weights=[left_weight, 10, right_weight], k=1)[0]
-                    else:
-                        movement = -1
+            new_pos = fish_state.fish_index + movement
 
-                    new_pos = fish_state.fish_index + movement
+            emoji[fish_state.fish_index], emoji[new_pos] = emoji[new_pos], emoji[fish_state.fish_index]
 
-                    emoji[fish_state.fish_index], emoji[new_pos] = emoji[new_pos], emoji[fish_state.fish_index]
+            fish_state.fish_index = new_pos
 
-                    fish_state.fish_index = new_pos
+        emoji_str = "\n".join(
+        "".join(emoji[i * const.fish_minigame_width:(i + 1) * const.fish_minigame_width])
+        for i in range(const.fish_minigame_height)
+        )
 
-                emoji_str = "\n".join(
-                "".join(emoji[i * const.fish_minigame_width:(i + 1) * const.fish_minigame_width])
-                for i in range(const.fish_minigame_height)
-                )
+        await msg.edit(content=emoji_str, view=view)
 
-                await msg.edit(content=emoji_str, view=view)
+        await asyncio.sleep(1)
 
-                await asyncio.sleep(1)
-
-            if not view.active:
-                await asyncio.sleep(10)
-            await msg.delete()
-        case "inv":
-            data = load_data()
-            user_id_str = str(interaction.user.id)
-
-            inv_msg_str = f"**Inventory** \n Common {fish_common}: {data[user_id_str]["Fish"]["Common"]} \n Uncommon {fish_uncommon}: {data[user_id_str]["Fish"]["Uncommon"]} \n Rare {fish_rare}: {data[user_id_str]["Fish"]["Rare"]} \n Legendary {fish_leg}: {data[user_id_str]["Fish"]["Legendary"]}"
-            await interaction.response.send_message(inv_msg_str)
-            inv_msg = await interaction.original_response()
-            await asyncio.sleep(20)
-            await inv_msg.delete()
+    if not view.active:
+        await asyncio.sleep(10)
+    await msg.delete()
 
 class Fish:
     def __init__(self, fish_index, fish_emoji):
@@ -310,6 +315,25 @@ class ReelIn(View):
             self.result = "Missed!"
             await interaction.edit_original_response(content="💨 The fish got away!", view=None)
 
+@client.tree.command(name="inventory", description="Display a players inventory", guild=GUILD_ID)
+async def inventory(interaction: discord.Interaction, user: discord.User = None):
+    data = load_data()
+    user_id_str = ""
+    user_name = ""
+    if user:
+        user_id_str = str(user.id)
+        user_name = user.display_name
+    else:
+        user_id_str = str(interaction.user.id)
+        user_name = interaction.user.display_name
+    msg = f"**{user_name}'s Inventory** \n Common {const.fish_common}: {data[user_id_str]["Fish"]["Common"]} \n Uncommon {const.fish_uncommon}: {data[user_id_str]["Fish"]["Uncommon"]} \n Rare {const.fish_rare}: {data[user_id_str]["Fish"]["Rare"]} \n Legendary {const.fish_leg}: {data[user_id_str]["Fish"]["Legendary"]}"
+    await sendMessageWithExpiration(interaction, msg, 40)
+
+async def sendMessageWithExpiration(interaction, msg_str, delay):
+    await interaction.response.send_message(msg_str)
+    msg = await interaction.original_response()
+    await asyncio.sleep(delay)
+    await msg.delete()
 
 @client.tree.command(name="casino", description="Casino", guild=GUILD_ID)
 @app_commands.describe(game="Casino game", bet_color="Pick color", amount="How much would you like to bet?")
@@ -319,7 +343,7 @@ async def casino(interaction: discord.Interaction, game: app_commands.Choice[str
     eph = True
 
     if amount > 0:
-        if checkAfford(interaction.user.id, amount):
+        if checkReq(interaction.user.id, amount, "Cash"):
             match game.value:
                 case "roulette":
                     n = random.randrange(0, 17)
@@ -350,13 +374,164 @@ async def casino(interaction: discord.Interaction, game: app_commands.Choice[str
     else:
         await interaction.response.send_message(f'You can not bet below 1', ephemeral=True)
    
+@client.tree.command(name="shop", description="Buy or sell items", guild=GUILD_ID)
+async def shop(interaction: discord.Interaction):
+    await interaction.response.send_message("Select a category:", view = ActionSelectView(), ephemeral = True)
+
+class ActionSelect(discord.ui.Select):
+    def __init__(self):
+        options = [
+            discord.SelectOption(label="Buy", description="Purchase items"),
+            discord.SelectOption(label="Sell", description="Sell items")
+        ]
+        super().__init__(placeholder="Buy or Sell?", options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        action = self.values[0]
+        await interaction.response.edit_message(content=f"You chose to **{action}**. Now select category:", view=CategorySelectView(action))
+
+class ActionSelectView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=30)
+        self.add_item(ActionSelect())
+
+class CategorySelect(discord.ui.Select):
+    def __init__(self, action):
+        self.action = action
+
+        available_categories = {
+            "Buy": ["Bait"],
+            "Sell": ["Fish"]
+        }
+
+        options = [
+            discord.SelectOption(label=cat, description=f"{action} {cat.lower()}")
+            for cat in available_categories.get(action, [])
+        ]
+
+        super().__init__(placeholder="Choose a category...", options = options)
+
+    async def callback(self, interaction: discord.Interaction):
+        category = self.values[0]
+        await interaction.response.edit_message(content=f"You selected **{category}**. Now choose an item to {self.action.lower()}:", view = ItemSelectView(self.action, category))
+
+class CategorySelectView(discord.ui.View):
+    def __init__(self, action):
+        super().__init__(timeout=30)
+        self.add_item(CategorySelect(action))
+
+class ItemSelect(discord.ui.Select):
+    def __init__(self, action, category):
+        self.action = action
+        self.category = category
+
+        if self.action == "Buy":
+            items = const.shop_items.get('Buy', {}).get(self.category, {})
+        elif self.action == "Sell":
+            items = const.shop_items.get('Sell', {}).get(self.category, {})
+        else:
+            items = {}
+
+        options = [discord.SelectOption(
+            label = item_name,
+            description = f"{'Cost' if action == "Buy" else 'Sell for'}: {price} cash"
+            )
+            for item_name, price in items.items()
+        ]
+        super().__init__(placeholder="Choose an item...", options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        selected_item = self.values[0]
+        category = self.category
+        action = self.action
+
+        await interaction.response.edit_message(content=f"You wish to {self.action.lower()} **{selected_item}**. How many would you like to {self.action.lower()}?", view = QuantitySelectView(action, category, selected_item, interaction.user.id))
+
+class ItemSelectView(discord.ui.View):
+    def __init__(self, action, category):
+        super().__init__(timeout=30)
+        self.add_item(ItemSelect(action, category))
+
+class QuantitySelect(discord.ui.Select):
+    def __init__(self, action, category, item, user_id):
+        self.action = action
+        self.category = category
+        self.item = item
+
+        data = load_data()
+        user_id = str(user_id)
+        self.price = const.shop_items[action][category][item]
+
+        if action == "Buy":
+            user_cash = data[user_id]["Cash"]
+            max_quantity = user_cash // self.price
+        elif action == "Sell":
+            max_quantity = data[user_id][category].get(item, 0)
+        else:
+            max_quantity = 0
+
+        quantities = range(1, min(max_quantity, 25) + 1)
+
+        if not quantities:
+            options = [discord.SelectOption(label="None Available", value="0")]
+        else:
+            options = [
+                discord.SelectOption(label=str(q), value=str(q), description=f"{q}x {item}")
+                for q in quantities
+            ]
+        
+        super().__init__(placeholder="Select quantity...", options=options)
+        self.user_id = user_id
+        self.item = item
+        self.category = category
+        self.action = action
+
+    async def callback(self, interaction: discord.Interaction):
+        quantity = int(self.values[0])
+        total_cost = self.price * quantity
+        data = load_data()
+        user_id = str(self.user_id)
+
+        if self.action =="Buy":
+            if data[user_id]["Cash"] >= total_cost:
+                data[user_id]["Cash"] -= total_cost
+                data[user_id][self.category][self.item] += quantity
+                save_data(data)
+                await interaction.response.edit_message(content=f"You bought {quantity}x {self.item} for {total_cost} cash!", view=None)
+            else:
+                await interaction.response.edit_message(content=f"Not enough cash", view=None)
+        elif self.action == "Sell":
+            if data[user_id][self.category][self.item] <= quantity:
+                data[user_id][self.category][self.item] -= quantity
+                data[user_id]["Cash"] += total_cost
+                save_data(data)
+                await interaction.response.edit_message(content=f"You sold {quantity}x {self.item} for {total_cost} cash!", view=None)
+            else:
+                await interaction.response.edit_message(content=f"Not enough {self.item}", view=None)
+
+class QuantitySelectView(discord.ui.View):
+    def __init__(self, action, category, item, user_id):
+        super().__init__(timeout=30)
+        self.add_item(QuantitySelect(action, category, item, user_id))
+
+
 def checkExist(user_id: int):
     data = load_data()
     user_id_str = str(user_id)
 
+    default_data = {"Cash": 10, "Level":0, "xp": 0, "Work": "00-01-01", "Fish": {"Common": 0, "Uncommon": 0, "Rare": 0, "Legendary": 0}, "Bait": {"Basic Bait": 0, "Advanced Bait": 0, "Master Bait": 0, "Mystery Bait": 0}}
+
     if user_id_str not in data:
-        data[user_id_str] = {"Cash": 10, "Level":0, "xp": 0, "Work": "00-01-01", "Fish": {"Common": 0, "Uncommon": 0, "Rare": 0, "Legendary": 0}}
-        save_data(data)
+        data[user_id_str] = default_data
+    else:
+        for key, value in default_data.items():
+            if key not in data[user_id_str]:
+                data[user_id_str][key] = value
+            elif isinstance(value, dict):
+                for sub_key, sub_value in value.items():
+                    if sub_key not in data[user_id_str][key]:
+                        data[user_id_str][key][sub_key] = sub_value
+    save_data(data)
 
 def incrementXp(id):
     checkExist(id)
@@ -367,15 +542,24 @@ def incrementXp(id):
 
     return checkLevelup(id, data)
 
-def checkAfford(id, req):
+def checkReq(id, req, check_str, sub_key = None):
     data = load_data()
     user_id_str = str(id)
 
-    if data[user_id_str]["Cash"] >= req:
-        updateMoney(id, -req)
-        return True
+    if sub_key:
+        if data[user_id_str][check_str][sub_key] >= req:
+            data[user_id_str][check_str][sub_key] -= req
+            save_data(data)
+            return True
+        else:
+            return False
     else:
-        return False
+        if data[user_id_str][check_str] >= req:
+            data[user_id_str][check_str] -= req
+            save_data(data)
+            return True
+        else:
+            return False
 
 def updateMoney(id, amount):
     data = load_data()
